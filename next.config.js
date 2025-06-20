f079827fe99d5856a605a87a4a6ef346c28a6464
kvnbryn <151827022+kvnbryn@users.noreply.github.com>
2025-06-20 21:13:09 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+// File: next.config.js
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  // TAMBAHKAN BARIS INI
+  output: 'standalone',
+
   experimental: {
     serverActions: {
       bodySizeLimit: '1000mb',
